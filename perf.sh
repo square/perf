eba7a0c1af0cda6727fb092d098122c82db4e8f7
Henry Qin <hqin@squareup.com>
2024-05-13 17:56:58 -0700

    Add VERBOSE_JVM options to perf.sh.
    When specified, this will make the JVM dump verbose output.

--- perf.sh
+++ perf.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 CLASSPATH_MAIN+=$(CP="build/main:build/gen"; for lib in `find lib -type f -name "*.jar"`; do CP="$CP:$lib"; done; printf "$CP" )
-JAVA_OPTIONS="-Xms6g -Xmx6g"
-java -cp "${CLASSPATH_MAIN}" $JAVA_OPTIONS \
+
+VERBOSE_JVM=${VERBOSE_JVM:-false}
+verbose_jvm_options=""
+if [[ "$VERBOSE_JVM" == "true" ]]; then
+  verbose_jvm_options="-XX:+PrintCompilation -verbose:gc"
+fi
+JVM_OPTIONS="${verbose_jvm_options} -Xms6g -Xmx6g ${JAVA_OPTIONS}"
+
+java -cp "${CLASSPATH_MAIN}" $JVM_OPTIONS "${PROXY_OPTIONS[@]}" \
 -Dorg.slf4j.simpleLogger.showDateTime=true \
 -Dorg.slf4j.simpleLogger.dateTimeFormat="yyyy-MM-dd'T'HH:mm:ss.SSSZ" \
 com.squareup.perf.Perf "$@"
